2a081be9f6977b86603ffa97ee06de1f5761d604
Kanak Varshney <kanakvarshney384@gmail.com>
2026-05-28 20:24:22 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int N, sum=0;
+    cout<<"enter the value of N : ";
+    cin>>N;
+    for(int i=1; i<=N; i++){
+        sum+=i;
+    }
+    cout<<"the sum of first "<<N<<" natural numbers is : "<<sum;
+    return 0;
+}
