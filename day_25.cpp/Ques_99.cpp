697924b20c19d9a5cad373824775d4836d2b986b
Kanak Varshney <kanakvarshney384@gmail.com>
2026-06-22 23:56:22 +0530

    Add files via upload

---
+++ day_25.cpp/Ques_99.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of names: ";
+    cin>>n;
+
+    string names[n];
+    cout<<"Enter names:\n";
+    for(int i=0;i<n;i++){
+        cin>>names[i];
+    }
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(names[i]>names[j]){
+                string temp=names[i];
+                names[i]=names[j];
+                names[j]=temp;
+            }
+        }
+    }
+    cout<<"\nNames in alphabetical order:\n";
+    for(int i=0;i<n;i++){
+        cout<<names[i]<<endl;
+    }
+}
